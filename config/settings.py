85af404bfabcd6bc73ac9687c2752e58a27fab75
pittlai <coclai@qq.com>
2022-07-30 14:18:52 +0800

    增加配置文件

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+
+
+class Config():
+    DEBUG = True
+    TESTING = False
+
+    DB_URI = "mysql://fanta:1551@192.168.174.130/python"
+
+    DB_HOST = '192.168.174.130',
+    DB_PORT = 3306,
+    DB_USER = 'fanta',
+    DB_PWD = '1551',
+    DB_DATABASE = 'python',
+
