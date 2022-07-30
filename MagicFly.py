1599d08050adfc15c4630a740f9c0c5b14440f88
pittlai <coclai@qq.com>
2022-07-30 14:27:25 +0800

    配置文件使用样例

--- MagicFly.py
+++ MagicFly.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 # import sqlHelper
 from dbHelper import db
 
 app = Flask(__name__)
+app.config.from_object('config.settings.Config')
+print(app.config['DB_HOST'])
 
 DATA_DIC = {
     1: {'name': '张三', 'age': 18},
     2: {'name': '李四', 'age': 25},
 }
+
 
 @app.route('/login')
 def login():
@@ -23,7 +26,6 @@ def login():
 def index():
     data_dict = DATA_DIC
     return render_template('index.html', data_dict=data_dict)
-
 
 
 @app.route("/order")
